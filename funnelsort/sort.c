fb56d2d98f1938ab2ff439a2ddea06544299fc06
Konstantin Osipov <kostja@tarantool.org>
2014-05-16 21:07:09 +0400

    Make the poor thing actually work.

--- funnelsort/sort.c
+++ funnelsort/sort.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 #include "sort.h"
 #define CACHELINE_SIZE 64
 
@@ -15,16 +16,16 @@ struct funnel {
 
 
 struct funnel *
-funnel_create(void *out, size_t nmemb, size_t size, cmp_t cmp)
+funnel_create(void *in, void *out, size_t nmemb, size_t size, cmp_t cmp)
 {
 	struct funnel *funnel = (struct funnel *)
 		malloc(sizeof(struct funnel));
+	funnel->in = in;
 	funnel->out = out;
 	funnel->nmemb = nmemb;
 	funnel->size = size;
 	funnel->pos = 0;
 	funnel->cmp = cmp;
-	funnel->in = NULL;
 
 	/*
 	 * Todo: here we must stop at sqrt(nmemb), not
@@ -35,10 +36,11 @@ funnel_create(void *out, size_t nmemb, size_t size, cmp_t cmp)
 	if (size * nmemb > CACHELINE_SIZE) {
 		size_t nmemb_left = nmemb/2;
 		size_t nmemb_right = nmemb - nmemb_left;
-		funnel->in = malloc(nmemb * size);
-		funnel->lr[0] = funnel_create(funnel->in, nmemb_left,
+		void *out = malloc(nmemb * size);
+		funnel->lr[0] = funnel_create(funnel->in, out, nmemb_left,
 					      size, cmp);
 		funnel->lr[1] = funnel_create(funnel->in + nmemb_left * size,
+					      out + nmemb_left * size,
 					      nmemb_right, size, cmp);
 	} else {
 		funnel->lr[0] = funnel->lr[1] = 0;
@@ -68,7 +70,8 @@ funnel_fill(struct funnel *funnel)
 	 * instead.
 	 */
 	if (funnel->lr[0] == NULL) {
-		qsort(funnel->out, funnel->nmemb, funnel->size, funnel->cmp);
+		qsort(funnel->in, funnel->nmemb, funnel->size, funnel->cmp);
+		memcpy(funnel->out, funnel->in, funnel->nmemb * funnel->size);
 		return;
 	}
 	/*
@@ -80,42 +83,43 @@ funnel_fill(struct funnel *funnel)
 
 	void *lr[2] = { NULL, NULL };
 	int res = 0, i = 0;
+	int size = funnel->size;
 
+	lr[res] = funnel_pop(funnel->lr[res]);
+	assert(lr[res]);
+	res = !res;
 	while (i < funnel->nmemb) {
-		while (lr[res] == NULL) {
-			lr[res] = funnel_pop(funnel->lr[res]);
-			if (lr[res] == NULL) {
-				/*
-				 * The sort buffer of one of the
-				 * funnels is exhausted. @todo:
-				 * here we will need to refill it.
-				 * Copy out the tail of the
-				 * "bigger" sub-funnel.
-				 */
-				res = !res;
-				/* Copy out the tail. */
-				while (i < funnel->nmemb) {
-					void *pos = (funnel->out + i++ *
-						     funnel->size);
-					memcpy(pos,
-					       funnel_pop(funnel->lr[res]),
-					       funnel->size);
-				}
-				return;
-			}
+		lr[res] = funnel_pop(funnel->lr[res]);
+		if (lr[res] == NULL) {
+			/*
+			 * The sort buffer of one of the
+			 * funnels is exhausted. @todo:
+			 * here we will need to refill it.
+			 */
 			res = !res;
+			/*
+			 * Copy out the tail of the
+			 * "bigger" sub-funnel.
+			 */
+			do {
+				memcpy(funnel->out + size * i++, lr[res], size);
+				lr[res] = funnel_pop(funnel->lr[res]);
+			} while (lr[res]);
+			assert(i == funnel->nmemb);
+			return;
 		}
 		res = funnel->cmp(lr[0], lr[1]) > 0 ? 1 : 0;
-		memcpy(funnel->out + i * funnel->size, lr[res], funnel->size);
+		memcpy(funnel->out + size * i++, lr[res], size);
 		lr[res] = NULL;
-		i++;
 	}
 }
 
 void
 sort(void *ptr, size_t nmemb, size_t size, cmp_t cmp)
 {
-	struct funnel *funnel = funnel_create(ptr, nmemb, size, cmp);
+	void *out = malloc(size * nmemb);
+	struct funnel *funnel = funnel_create(ptr, out, nmemb, size, cmp);
 	funnel_fill(funnel);
+	memcpy(ptr, out, size * nmemb);
 }
 
