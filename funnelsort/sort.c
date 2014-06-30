5aa0a4e341c420783aff2e78e697ae83e4b97a65
Konstantin Osipov <kostja@tarantool.org>
2014-06-30 21:41:16 +0400

    Mixed changes in funnel sort.

--- funnelsort/sort.c
+++ funnelsort/sort.c
@@ -4,14 +4,25 @@
 #include "sort.h"
 #define CACHELINE_SIZE 64
 
+/** struct funnel - represents a top-level funnel,
+ * a funnel leaf and a binary merger.
+ */
 struct funnel {
+	/** left and right funnels of the binary mergers */
 	struct funnel  *lr[2];
+	/** merger output */
 	void *out;
+	/** merger input - for a leaf funnel.  */
 	void *in;
+	/** element size */
 	int size;
+	/** the total number of elements */
 	int nmemb;
+	/** if the funnel is populated, the position in output,
+	 * */
 	int pos;
 	cmp_t cmp;
+	bool exhausted;
 };
 
 
@@ -24,7 +35,6 @@ funnel_create(void *in, void *out, size_t nmemb, size_t size, cmp_t cmp)
 	funnel->out = out;
 	funnel->nmemb = nmemb;
 	funnel->size = size;
-	funnel->pos = 0;
 	funnel->cmp = cmp;
 
 	/*
@@ -33,15 +43,17 @@ funnel_create(void *in, void *out, size_t nmemb, size_t size, cmp_t cmp)
 	 * create a sqrt(sqrt(nmemb))-way sub-funnel for each
 	 * leaf, and so on, until we reach CACHELINE_SIZE.
 	 */
-	if (size * nmemb > CACHELINE_SIZE) {
+	if (size * nmemb > K) {
 		size_t nmemb_left = nmemb/2;
 		size_t nmemb_right = nmemb - nmemb_left;
-		void *out = malloc(nmemb * size);
+		void *out = malloc(K * lg(nmemb) * size);
 		funnel->lr[0] = funnel_create(funnel->in, out, nmemb_left,
 					      size, cmp);
 		funnel->lr[1] = funnel_create(funnel->in + nmemb_left * size,
 					      out + nmemb_left * size,
 					      nmemb_right, size, cmp);
+	} else if (size * nmemb > CACHELINE_SIZE) {
+		K = pow(nmemb, 1/3.0)
 	} else {
 		funnel->lr[0] = funnel->lr[1] = 0;
 	}
@@ -51,8 +63,22 @@ funnel_create(void *in, void *out, size_t nmemb, size_t size, cmp_t cmp)
 void *
 funnel_pop(struct funnel *funnel)
 {
-	if (funnel->pos >= funnel->nmemb)
+	if (funnel->exhausted)
 		return NULL;
+	if (funnel->pos >= funnel->nmemb) {
+		if (funnel->lr[0] == NULL && funnel->lr[1] == NULL) {
+			/* leaf funnel */
+			funnel->exhausted = true;
+			return NULL;
+		}
+		if (funnel->lr[0]->exhausted && funnel->lr[1]->exhausted) {
+			funnel->exhausted = true;
+			return NULL;
+		}
+		if (funnel->lr[0]->exhausted || funnel->lr[1]->exhausted)
+			return funnel_pop(funnel->lr[!funnel->lr[0]->exhausted]);
+		funnel_fill(funnel);
+	}
 	return funnel->out + funnel->pos++ * funnel->size;
 }
 
@@ -64,19 +90,22 @@ funnel_fill(struct funnel *funnel)
 	 * and pre-processing for input (swapping in.)
 	 */
 	/*
-	 * Case 1: this is a leaf funnel.
+	 * Case 1: this is a leaf.
 	 * Sort the output.
-	 * @todo: (re-)fill the leaf funnel on demand in funnel_pop()
-	 * instead.
 	 */
 	if (funnel->lr[0] == NULL) {
 		qsort(funnel->in, funnel->nmemb, funnel->size, funnel->cmp);
-		memcpy(funnel->out, funnel->in, funnel->nmemb * funnel->size);
+		funnel->out = funnel->in;
 		return;
 	}
 	/*
-	 * Case 2: this is an intermediate funnel.
-	 * Sort the children and merge them.
+	 * Case 2: this is a top merger of some sub-funnel.
+	 * Nothing needs to be done, it will be re-filled on
+	 * demand in funnel_pop().
+	 */
+	/*
+	 * Case 3: this is an intermediate funnel.
+	 * Fill the children and merge them.
 	 */
 	funnel_fill(funnel->lr[0]);
 	funnel_fill(funnel->lr[1]);
@@ -112,6 +141,7 @@ funnel_fill(struct funnel *funnel)
 		memcpy(funnel->out + size * i++, lr[res], size);
 		lr[res] = NULL;
 	}
+	funnel->pos = 0;
 }
 
 void
@@ -119,7 +149,10 @@ sort(void *ptr, size_t nmemb, size_t size, cmp_t cmp)
 {
 	void *out = malloc(size * nmemb);
 	struct funnel *funnel = funnel_create(ptr, out, nmemb, size, cmp);
-	funnel_fill(funnel);
+	while (! funnel->exhausted) {
+		funnel_fill(funnel);
+		memcpy(out, funnel->out, funnel->nmemb * funnel->size);
+	}
 	memcpy(ptr, out, size * nmemb);
 }
 
